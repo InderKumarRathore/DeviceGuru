15dfce7b30ba5b4e0fffe6ebe0ea38b86df99bf3
Inder Kumar Rathore <inderkumarrathore@Rathore.local>
2015-04-10 11:05:22 +0530

    Release 1.1.0

---
+++ DeviceGuru.h
@@ -0,0 +1,11 @@
+//
+//  DeviceGuru.h
+//
+//  Created by Inder Kumar Rathore on 06/02/15.
+//  Copyright (c) 2015. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double DeviceGuruVersionNumber;
+FOUNDATION_EXPORT const unsigned char DeviceGuruVersionString[];
