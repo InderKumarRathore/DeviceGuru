c16dc194822dd00e97422eb5de15cde8fdaf5473
Han <yanhan02008@gmail.com>
2015-09-23 23:05:49 -0500

    ADD default

--- DeviceGuru.swift
+++ DeviceGuru.swift
@@ -266,6 +266,8 @@ public func hardwareNumber(hardware: Hardware) -> CGFloat {
     
   case Hardware.SIMULATOR:                         return 100.0
   case Hardware.NOT_AVAILABLE:                     return 200.0
+  
+  default:                                         return -1.0
   }
 }
 
