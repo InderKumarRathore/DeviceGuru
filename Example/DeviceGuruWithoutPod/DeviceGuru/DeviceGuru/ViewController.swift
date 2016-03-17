d2dcdc3f7d0573626266a584aaaed4d414cd10fc
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2016-03-17 22:50:07 +0530

    Removed test project from it

---
+++ Example/DeviceGuruWithoutPod/DeviceGuru/DeviceGuru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceGuru
+//
+//  Created by Inder Kumar Rathore on 17/03/16.
+//  Copyright © 2016 Inder Kumar Rathore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
