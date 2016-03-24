30547a0d77fe1e168f26f92aa869bd535170533a
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2016-03-24 17:24:26 +0530

    Moved Hardware enum to different file

---
+++ Hardware.swift
@@ -0,0 +1,76 @@
+//
+//  Hardware.swift
+//  DeviceGuru
+//
+//  Created by Inder Kumar Rathore on 24/03/16.
+//  Copyright © 2016 Inder Kumar Rathore. All rights reserved.
+//
+
+
+/// Enum for different iPhone/iPad devices
+public enum Hardware {
+  case NOT_AVAILABLE
+  
+  case IPHONE_2G
+  case IPHONE_3G
+  case IPHONE_3GS
+  
+  case IPHONE_4
+  case IPHONE_4_CDMA
+  case IPHONE_4S
+  
+  case IPHONE_5
+  case IPHONE_5_CDMA_GSM
+  case IPHONE_5C
+  case IPHONE_5C_CDMA_GSM
+  case IPHONE_5S
+  case IPHONE_5S_CDMA_GSM
+  
+  case IPHONE_6
+  case IPHONE_6_PLUS
+  case IPHONE_6S
+  case IPHONE_6S_PLUS
+  case IPHONE_SE
+  
+  case IPOD_TOUCH_1G
+  case IPOD_TOUCH_2G
+  case IPOD_TOUCH_3G
+  case IPOD_TOUCH_4G
+  case IPOD_TOUCH_5G
+  case IPOD_TOUCH_6G
+  
+  case IPAD
+  case IPAD_2
+  case IPAD_2_WIFI
+  case IPAD_2_CDMA
+  case IPAD_3
+  case IPAD_3G
+  case IPAD_3_WIFI
+  case IPAD_3_WIFI_CDMA
+  case IPAD_4
+  case IPAD_4_WIFI
+  case IPAD_4_GSM_CDMA
+  
+  case IPAD_MINI
+  case IPAD_MINI_WIFI
+  case IPAD_MINI_WIFI_CDMA
+  case IPAD_MINI_RETINA_WIFI
+  case IPAD_MINI_RETINA_WIFI_CDMA
+  case IPAD_MINI_3_WIFI
+  case IPAD_MINI_3_WIFI_CELLULAR
+  case IPAD_MINI_3_WIFI_CELLULAR_CN
+  case IPAD_MINI_4_WIFI
+  case IPAD_MINI_4_WIFI_CELLULAR
+  case IPAD_MINI_RETINA_WIFI_CELLULAR_CN
+  
+  case IPAD_AIR_WIFI
+  case IPAD_AIR_WIFI_GSM
+  case IPAD_AIR_WIFI_CDMA
+  case IPAD_AIR_2_WIFI
+  case IPAD_AIR_2_WIFI_CELLULAR
+  
+  case IPAD_PRO_WIFI
+  case IPAD_PRO_WIFI_CELLULAR
+  
+  case SIMULATOR
+}
