c9f89e40241e4a54b8d910f2b9de5734b18ee4dc
Damien Legrand <legrand.damien.06@gmail.com>
2017-04-02 12:24:59 +0200

    New hardwares
    - 9.7-inch iPad (2017)
    - Apple TV (1G / 2G / 3G / 3.2G / 4G)
    - Apple Watch (1G / Series 1 / Series 2)

--- Hardware.swift
+++ Hardware.swift
@@ -9,73 +9,89 @@
 
 /// Enum for different iPhone/iPad devices
 public enum Hardware {
-  case not_AVAILABLE
-  
-  case iphone_2G
-  case iphone_3G
-  case iphone_3GS
-  
-  case iphone_4
-  case iphone_4_CDMA
-  case iphone_4S
-  
-  case iphone_5
-  case iphone_5_CDMA_GSM
-  case iphone_5C
-  case iphone_5C_CDMA_GSM
-  case iphone_5S
-  case iphone_5S_CDMA_GSM
-  
-  case iphone_6
-  case iphone_6_PLUS
-  case iphone_6S
-  case iphone_6S_PLUS
-  case iphone_SE
-  
-  case iphone_7
-  case iphone_7_PLUS
-  
-  case ipod_TOUCH_1G
-  case ipod_TOUCH_2G
-  case ipod_TOUCH_3G
-  case ipod_TOUCH_4G
-  case ipod_TOUCH_5G
-  case ipod_TOUCH_6G
-  
-  case ipad
-  case ipad_2
-  case ipad_2_WIFI
-  case ipad_2_CDMA
-  case ipad_3
-  case ipad_3G
-  case ipad_3_WIFI
-  case ipad_3_WIFI_CDMA
-  case ipad_4
-  case ipad_4_WIFI
-  case ipad_4_GSM_CDMA
-  
-  case ipad_MINI
-  case ipad_MINI_WIFI
-  case ipad_MINI_WIFI_CDMA
-  case ipad_MINI_RETINA_WIFI
-  case ipad_MINI_RETINA_WIFI_CDMA
-  case ipad_MINI_3_WIFI
-  case ipad_MINI_3_WIFI_CELLULAR
-  case ipad_MINI_3_WIFI_CELLULAR_CN
-  case ipad_MINI_4_WIFI
-  case ipad_MINI_4_WIFI_CELLULAR
-  case ipad_MINI_RETINA_WIFI_CELLULAR_CN
-  
-  case ipad_AIR_WIFI
-  case ipad_AIR_WIFI_GSM
-  case ipad_AIR_WIFI_CDMA
-  case ipad_AIR_2_WIFI
-  case ipad_AIR_2_WIFI_CELLULAR
-  
-  case ipad_PRO_97_WIFI
-  case ipad_PRO_97_WIFI_CELLULAR
-  case ipad_PRO_WIFI
-  case ipad_PRO_WIFI_CELLULAR
-  
-  case simulator
+    case not_AVAILABLE
+
+    case iphone_2G
+    case iphone_3G
+    case iphone_3GS
+
+    case iphone_4
+    case iphone_4_CDMA
+    case iphone_4S
+
+    case iphone_5
+    case iphone_5_CDMA_GSM
+    case iphone_5C
+    case iphone_5C_CDMA_GSM
+    case iphone_5S
+    case iphone_5S_CDMA_GSM
+
+    case iphone_6
+    case iphone_6_PLUS
+    case iphone_6S
+    case iphone_6S_PLUS
+    case iphone_SE
+
+    case iphone_7
+    case iphone_7_PLUS
+
+    case ipod_TOUCH_1G
+    case ipod_TOUCH_2G
+    case ipod_TOUCH_3G
+    case ipod_TOUCH_4G
+    case ipod_TOUCH_5G
+    case ipod_TOUCH_6G
+
+    case ipad
+    case ipad_2
+    case ipad_2_WIFI
+    case ipad_2_CDMA
+    case ipad_3
+    case ipad_3G
+    case ipad_3_WIFI
+    case ipad_3_WIFI_CDMA
+    case ipad_4
+    case ipad_4_WIFI
+    case ipad_4_GSM_CDMA
+
+    case ipad_MINI
+    case ipad_MINI_WIFI
+    case ipad_MINI_WIFI_CDMA
+    case ipad_MINI_RETINA_WIFI
+    case ipad_MINI_RETINA_WIFI_CDMA
+    case ipad_MINI_3_WIFI
+    case ipad_MINI_3_WIFI_CELLULAR
+    case ipad_MINI_3_WIFI_CELLULAR_CN
+    case ipad_MINI_4_WIFI
+    case ipad_MINI_4_WIFI_CELLULAR
+    case ipad_MINI_RETINA_WIFI_CELLULAR_CN
+
+    case ipad_AIR_WIFI
+    case ipad_AIR_WIFI_GSM
+    case ipad_AIR_WIFI_CDMA
+    case ipad_AIR_2_WIFI
+    case ipad_AIR_2_WIFI_CELLULAR
+
+    case ipad_PRO_97_WIFI
+    case ipad_PRO_97_WIFI_CELLULAR
+    case ipad_PRO_WIFI
+    case ipad_PRO_WIFI_CELLULAR
+    
+    case ipad_2017_WIFI
+    case ipad_2017_WIFI_CELLULAR
+
+    case appleTV_1G
+    case appleTV_2G
+    case appleTV_3G
+    case appleTV_3_2G
+    case appleTV_4G
+    
+    case appleWatch_38
+    case appleWatch_42
+    case appleWatch_SERIES_2_38
+    case appleWatch_SERIES_2_42
+    case appleWatch_SERIES_1_38
+    case appleWatch_SERIES_1_42
+
+    case simulator
 }
