ea16a1cca4fde663db731291635a370af5bf2d85
Hannes Oud <hannes.oud.dev@gmail.com>
2017-06-29 17:07:29 +0200

    Fix Simultaneous accesses to var warnings / errors with Swift 3.2 and Swift 4

--- DeviceGuru.swift
+++ DeviceGuru.swift
@@ -20,9 +20,9 @@ open class DeviceGuru {
   class open func hardwareString() -> String {
     var name: [Int32] = [CTL_HW, HW_MACHINE]
     var size: Int = 2
-    sysctl(&name, 2, nil, &size, &name, 0)
+    sysctl(&name, 2, nil, &size, nil, 0)
     var hw_machine = [CChar](repeating: 0, count: Int(size))
-    sysctl(&name, 2, &hw_machine, &size, &name, 0)
+    sysctl(&name, 2, &hw_machine, &size, nil, 0)
     
     let hardware: String = String(cString: hw_machine)
     return hardware
