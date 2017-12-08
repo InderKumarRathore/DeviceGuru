e6082f84c446e1100a15ee33afb930fcb19fb1e3
Jeff Scaturro <jeffrey.scaturro@gmail.com>
2017-12-08 09:56:56 -0800

    Addresses #46 and adds iPhone 8 & X to readme.

--- DeviceGuru.swift
+++ DeviceGuru.swift
@@ -11,10 +11,10 @@ import Foundation
 import UIKit
 
 open class DeviceGuru {
-  
+
   /// Stores the list of the devices from the DeviceList.plist
   private let deviceListDict: [String: AnyObject]
-  
+
   /// Initialises the DeviceGuru using DeviceList.plist if the plist is not found then it asserts
   public init() {
     // get the bundle of the DeviceUtil if it's main bundle then it returns main bundle
@@ -41,7 +41,7 @@ open class DeviceGuru {
       self.deviceListDict = [String: AnyObject]()
     }
   }
-  
+
   /// This method returns the hardware type
   ///
   ///
@@ -53,11 +53,11 @@ open class DeviceGuru {
     sysctl(&name, 2, nil, &size, nil, 0)
     var hw_machine = [CChar](repeating: 0, count: Int(size))
     sysctl(&name, 2, &hw_machine, &size, nil, 0)
-    
+
     let hardware: String = String(cString: hw_machine)
     return hardware
   }
-  
+
   /// This method returns the Hardware enum depending upon harware string
   ///
   ///
@@ -65,53 +65,53 @@ open class DeviceGuru {
   ///
   public func hardware() -> Hardware {
     let hardware = hardwareString()
-    
+
     if (hardware == "iPhone1,1")         { return Hardware.iphone2g }
     if (hardware == "iPhone1,2")         { return Hardware.iphone3g }
     if (hardware == "iPhone2,1")         { return Hardware.iphone3gs }
-    
+
     if (hardware == "iPhone3,1")         { return Hardware.iphone4 }
     if (hardware == "iPhone3,2")         { return Hardware.iphone4 }
     if (hardware == "iPhone3,3")         { return Hardware.iphone4_cdma }
     if (hardware == "iPhone4,1")         { return Hardware.iphone4s }
-    
+
     if (hardware == "iPhone5,1")         { return Hardware.iphone5 }
     if (hardware == "iPhone5,2")         { return Hardware.iphone5_cdma_gsm }
     if (hardware == "iPhone5,3")         { return Hardware.iphone5c }
     if (hardware == "iPhone5,4")         { return Hardware.iphone5c_cdma_gsm }
     if (hardware == "iPhone6,1")         { return Hardware.iphone5s }
     if (hardware == "iPhone6,2")         { return Hardware.iphone5s_cdma_gsm }
-    
+
     if (hardware == "iPhone7,1")         { return Hardware.iphone6Plus }
     if (hardware == "iPhone7,2")         { return Hardware.iphone6 }
     if (hardware == "iPhone8,1")         { return Hardware.iphone6s }
     if (hardware == "iPhone8,2")         { return Hardware.iphone6sPlus }
     if (hardware == "iPhone8,4")         { return Hardware.iphoneSE }
-    
+
     if (hardware == "iPhone9,1")         { return Hardware.iphone7 }
     if (hardware == "iPhone9,2")         { return Hardware.iphone7Plus }
     if (hardware == "iPhone9,3")         { return Hardware.iphone7 }
     if (hardware == "iPhone9,4")         { return Hardware.iphone7Plus }
 
-    if (hardware == "iPhone10,1")         { return Hardware.iphone8_cn }
-    if (hardware == "iPhone10,2")         { return Hardware.iphone8Plus_cn }
-    if (hardware == "iPhone10,3")         { return Hardware.iphoneX_cn }
+    if (hardware == "iPhone10,1")         { return Hardware.iphone8 }
+    if (hardware == "iPhone10,2")         { return Hardware.iphone8Plus }
+    if (hardware == "iPhone10,3")         { return Hardware.iphoneX }
     if (hardware == "iPhone10,4")         { return Hardware.iphone8 }
     if (hardware == "iPhone10,5")         { return Hardware.iphone8Plus }
     if (hardware == "iPhone10,6")         { return Hardware.iphoneX }
-    
-    
-    
+
+
+
     if (hardware == "iPod1,1")           { return Hardware.ipodTouch1g }
     if (hardware == "iPod2,1")           { return Hardware.ipodTouch2g }
     if (hardware == "iPod3,1")           { return Hardware.ipodTouch3g }
     if (hardware == "iPod4,1")           { return Hardware.ipodTouch4g }
     if (hardware == "iPod5,1")           { return Hardware.ipodTouch5g }
     if (hardware == "iPod7,1")           { return Hardware.ipodTouch6g }
-    
+
     if (hardware == "iPad1,1")           { return Hardware.ipad }
     if (hardware == "iPad1,2")           { return Hardware.ipad3g }
-    
+
     if (hardware == "iPad2,1")           { return Hardware.ipad2_wifi }
     if (hardware == "iPad2,2")           { return Hardware.ipad2 }
     if (hardware == "iPad2,3")           { return Hardware.ipad2_cdma }
@@ -119,14 +119,14 @@ open class DeviceGuru {
     if (hardware == "iPad2,5")           { return Hardware.ipadMini_wifi }
     if (hardware == "iPad2,6")           { return Hardware.ipadMini }
     if (hardware == "iPad2,7")           { return Hardware.ipadMini_wifi_cdma }
-    
+
     if (hardware == "iPad3,1")           { return Hardware.ipad3_wifi }
     if (hardware == "iPad3,2")           { return Hardware.ipad3_wifi_cdma }
     if (hardware == "iPad3,3")           { return Hardware.ipad3 }
     if (hardware == "iPad3,4")           { return Hardware.ipad4_wifi }
     if (hardware == "iPad3,5")           { return Hardware.ipad4 }
     if (hardware == "iPad3,6")           { return Hardware.ipad4_gsm_cdma }
-    
+
     if (hardware == "iPad4,1")           { return Hardware.ipadAir_wifi }
     if (hardware == "iPad4,2")           { return Hardware.ipadAir_wifi_gsm }
     if (hardware == "iPad4,3")           { return Hardware.ipadAir_wifi_cdma }
@@ -136,85 +136,85 @@ open class DeviceGuru {
     if (hardware == "iPad4,7")           { return Hardware.ipadMini3_wifi }
     if (hardware == "iPad4,8")           { return Hardware.ipadMini3_wifi_cellular }
     if (hardware == "iPad4,9")           { return Hardware.ipadMini3_wifi_cellular_cn }
-    
+
     if (hardware == "iPad5,1")           { return Hardware.ipadMini4_wifi }
     if (hardware == "iPad5,2")           { return Hardware.ipadMini4_wifi_cellular }
     if (hardware == "iPad5,3")           { return Hardware.ipadAir2_wifi }
     if (hardware == "iPad5,4")           { return Hardware.ipadAir2_wifi_cellular }
-    
+
     if (hardware == "iPad6,3")           { return Hardware.ipadPro_97_wifi }
     if (hardware == "iPad6,4")           { return Hardware.ipadPro_97_wifi_cellular }
     if (hardware == "iPad6,7")           { return Hardware.ipadPro_wifi }
     if (hardware == "iPad6,8")           { return Hardware.ipadPro_wifi_cellular }
     if (hardware == "iPad6,11")          { return Hardware.ipad5_wifi }
     if (hardware == "iPad6,12")          { return Hardware.ipad5_wifi_cellular }
-    
+
     if (hardware == "iPad7,1")           { return Hardware.ipadPro2g_wifi }
     if (hardware == "iPad7,2")           { return Hardware.ipadPro2g_wifi_cellular }
     if (hardware == "iPad7,3")           { return Hardware.ipadPro_105_wifi }
     if (hardware == "iPad7,4")           { return Hardware.ipadPro_105_wifi_cellular }
-    
+
     if (hardware == "AppleTV1,1")        { return Hardware.appleTv1g }
     if (hardware == "AppleTV2,1")        { return Hardware.appleTv2g }
     if (hardware == "AppleTV3,1")        { return Hardware.appleTv3g_2012 }
     if (hardware == "AppleTV3,2")        { return Hardware.appleTv3g_2013 }
     if (hardware == "AppleTV5,3")        { return Hardware.appleTv4g }
-    
+
     if (hardware == "Watch1,1")          { return Hardware.appleWatch_38 }
     if (hardware == "Watch1,2")          { return Hardware.appleWatch_42 }
     if (hardware == "Watch2,3")          { return Hardware.appleWatch_series_2_38 }
     if (hardware == "Watch2,4")          { return Hardware.appleWatch_series_2_42 }
     if (hardware == "Watch2,6")          { return Hardware.appleWatch_series_1_38 }
     if (hardware == "Watch2,7")          { return Hardware.appleWatch_series_1_42 }
-    
+
     if (hardware == "i386")              { return Hardware.simulator }
     if (hardware == "x86_64")            { return Hardware.simulator }
     if (hardware.hasPrefix("iPhone"))    { return Hardware.simulator }
     if (hardware.hasPrefix("iPod"))      { return Hardware.simulator }
     if (hardware.hasPrefix("iPad"))      { return Hardware.simulator }
-    
+
     //log message that your device is not present in the list
     logMessage(hardware)
-    
+
     return Hardware.notAvailable
   }
-  
+
   /// This method returns the Platform enum depending upon harware string
   ///
   ///
   /// - returns: `Platform` type of the device
   ///
   public func platform() -> Platform {
-    
+
     let hardware = hardwareString().lowercased()
-    
+
     if hardware.hasPrefix(Platform.iPhone.rawValue) { return Platform.iPhone }
     if hardware.hasPrefix(Platform.iPodTouch.rawValue) { return Platform.iPodTouch }
     if hardware.hasPrefix(Platform.iPad.rawValue) { return Platform.iPad }
     if hardware.hasPrefix(Platform.appleTV.rawValue) { return Platform.appleTV }
     if hardware.hasPrefix(Platform.appleWatch.rawValue) { return Platform.appleWatch }
-    
+
     return Platform.unknown
   }
-  
-  
+
+
   /// This method returns the readable description of hardware string
   ///
   /// - returns: readable description `String` of the device
   ///
   public func hardwareDescription() -> String? {
     let hardware = hardwareString()
-    
+
     let hardwareDetail = self.deviceListDict[hardware] as? [String: AnyObject]
     if let hardwareDescription = hardwareDetail?["name"] {
       return hardwareDescription as? String
     }
-    
+
     //log message that your device is not present in the list
     logMessage(hardware)
     return nil
   }
-  
+
   /// This method returns the hardware number not actual but logically.
   /// e.g. if the hardware string is 5,1 then hardware number would be 5.1
   ///
@@ -225,14 +225,14 @@ open class DeviceGuru {
       if let hardwareNumber = hardwareDetail?["version"] as? Float {
         return hardwareNumber
       }
-    
+
     //log message that your device is not present in the list
     logMessage(hardware)
-    
+
     return 200.0 //device might be new one of missing one so returning 200.0f
   }
-  
-  
+
+
   /// This method returns the resolution for still image that can be received
   /// from back camera of the current device. Resolution returned for image oriented landscape right.
   ///
@@ -245,54 +245,54 @@ open class DeviceGuru {
     switch (hardware) {
     case .iphone2g, .iphone3g:
       return CGSize(width: 1600, height: 1200)
-      
+
     case .iphone3gs:
       return CGSize(width: 2048, height: 1536)
-      
+
     case .iphone4, .iphone4_cdma, .ipad3_wifi, .ipad3_wifi_cdma, .ipad3, .ipad4_wifi, .ipad4, .ipad4_gsm_cdma:
       return CGSize(width: 2592, height: 1936)
-      
+
     case .iphone4s, .iphone5, .iphone5_cdma_gsm, .iphone5c, .iphone5c_cdma_gsm, .iphone6, .iphone6Plus:
       return CGSize(width: 3264, height: 2448)
-      
+
     case .ipodTouch4g:
       return CGSize(width: 960, height: 720)
-      
+
     case .ipodTouch5g:
       return CGSize(width: 2440, height: 1605)
-      
+
     case .ipad2_wifi, .ipad2, .ipad2_cdma:
       return CGSize(width: 872, height: 720)
-      
+
     case .ipadMini_wifi, .ipadMini, .ipadMini_wifi_cdma:
       return CGSize(width: 1820, height: 1304)
-      
+
     case .ipadMini4_wifi, .ipadMini4_wifi_cellular:
       return CGSize(width: 3264, height: 2448)
-      
+
     case .ipadAir2_wifi, .ipadAir2_wifi_cellular:
       return CGSize(width: 2048, height: 1536)
-      
+
     case .iphone6s, .iphone6sPlus, .iphoneSE, .iphone7, .iphone7Plus:
       return CGSize(width: 4032, height: 3024)
-      
+
     case .ipadPro_97_wifi, .ipadPro_97_wifi_cellular:
       return CGSize(width: 4032, height: 3024)
-      
+
     case .ipadPro_wifi, .ipadPro_wifi_cellular:
       return CGSize(width: 3264, height: 2448)
-      
+
     case .ipadPro2g_wifi, .ipadPro2g_wifi_cellular, .ipadPro_105_wifi, .ipadPro_105_wifi_cellular:
       return CGSize(width: 4032, height: 3024)
-      
+
     default:
       print("We have no resolution for your device's camera listed in this category. Please, take photo with back camera of your device, get its resolution in pixels (via Preview Cmd+I for example) and add a comment to this repository (https://github.com/InderKumarRathore/DeviceGuru) on GitHub.com in format Device = Wpx x Hpx.")
     }
     print("Your device is: \(hardwareDescription() ?? "unknown")")
-    
+
     return CGSize.zero
   }
-  
+
   /// Internal method for loggin, you don't need this method
   ///
   /// - parameters:
@@ -303,5 +303,3 @@ open class DeviceGuru {
     print("Your device hardware string is: %@", hardware)
   }
 }
-
-
--- Hardware.swift
+++ Hardware.swift
@@ -11,45 +11,42 @@
 /// Enum for different iPhone/iPad devices
 public enum Hardware {
   case notAvailable
-  
+
   case iphone2g
   case iphone3g
   case iphone3gs
-  
+
   case iphone4
   case iphone4_cdma
   case iphone4s
-  
+
   case iphone5
   case iphone5_cdma_gsm
   case iphone5c
   case iphone5c_cdma_gsm
   case iphone5s
   case iphone5s_cdma_gsm
-  
+
   case iphone6
   case iphone6Plus
   case iphone6s
   case iphone6sPlus
   case iphoneSE
-  
+
   case iphone7
   case iphone7Plus
-  
+
   case iphone8
-  case iphone8_cn
   case iphone8Plus
-  case iphone8Plus_cn
   case iphoneX
-  case iphoneX_cn
-  
+
   case ipodTouch1g
   case ipodTouch2g
   case ipodTouch3g
   case ipodTouch4g
   case ipodTouch5g
   case ipodTouch6g
-  
+
   case ipad
   case ipad2
   case ipad2_wifi
@@ -61,7 +58,7 @@ public enum Hardware {
   case ipad4
   case ipad4_wifi
   case ipad4_gsm_cdma
-  
+
   case ipadMini
   case ipadMini_wifi
   case ipadMini_wifi_cdma
@@ -73,39 +70,39 @@ public enum Hardware {
   case ipadMini4_wifi
   case ipadMini4_wifi_cellular
   case ipadMiniRetina_wifi_cellular_cn
-  
+
   case ipadAir_wifi
   case ipadAir_wifi_gsm
   case ipadAir_wifi_cdma
   case ipadAir2_wifi
   case ipadAir2_wifi_cellular
-  
+
   case ipadPro_97_wifi
   case ipadPro_97_wifi_cellular
   case ipadPro_wifi
   case ipadPro_wifi_cellular
-  
+
   case ipad5_wifi
   case ipad5_wifi_cellular
-  
+
   case ipadPro2g_wifi
   case ipadPro2g_wifi_cellular
   case ipadPro_105_wifi
   case ipadPro_105_wifi_cellular
-  
+
   case appleTv1g
   case appleTv2g
   case appleTv3g_2012
   case appleTv3g_2013
   case appleTv4g
-  
+
   case appleWatch_38
   case appleWatch_42
   case appleWatch_series_2_38
   case appleWatch_series_2_42
   case appleWatch_series_1_38
   case appleWatch_series_1_42
-  
+
   case simulator
 }
 
