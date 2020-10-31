a15498ca94d6195a7caf68428ff0f19f172c450c
Witek Bobrowski <witek@bobrowski.com.pl>
2020-10-31 19:49:17 +0100

    Add simple example App

---
+++ Example/DeviceGuruExample/DeviceGuruExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceGuruExampleApp.swift
+//  DeviceGuruExample
+//
+//  Created by Witek Bobrowski on 31/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DeviceGuruExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
