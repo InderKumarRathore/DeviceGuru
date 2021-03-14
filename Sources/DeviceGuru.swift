3eaa9ebe7b9aa9404e6fda03255a664000f89469
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2021-03-14 20:30:31 +0700

    Used user defaults to read the verion

---
+++ Sources/DeviceGuru.swift
@@ -0,0 +1,31 @@
+//
+//  DeviceGuru.swift
+//  DeviceGuru
+//
+//  Created by Inder Kumar Rathore on 28/12/20.
+//
+
+import Foundation
+
+public protocol DeviceGuru {
+
+    /// Version of the library
+    static var version: String { get }
+
+    /// Returns raw `String` of device type, e.g. iPhone5,1
+    var hardwareString: String { get }
+
+    var platform: Platform { get }
+
+    var hardware: Hardware { get }
+
+    /// This method returns the hardware version not actual but logical.
+    /// e.g. iPhone5,11 will return `DeviceVersion(major: 5, minor: 11)`
+    func deviceVersion() throws -> DeviceVersion
+
+    /// Returns a readable description of the hardware string without including device variants related to wireless or cellular networking.
+    func hardwareSimpleDescription() throws -> String
+
+    /// Returns readable description `String` of the device
+    func hardwareDescription() throws -> String
+}
