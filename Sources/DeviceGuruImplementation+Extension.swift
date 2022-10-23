0fed860f19de96c01ca18a880095e3cb494717b5
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2022-10-23 23:06:00 +0800

    Updated the tests

--- Sources/DeviceGuruImplementation+Extension.swift
+++ Sources/DeviceGuruImplementation+Extension.swift
@@ -2,7 +2,7 @@
 public extension DeviceGuruImplementation {
 
     /// This should be same as cocoa pod version
-    static var libraryVersion: String { "<#Major#>.<#Minor#>.<#Fixes#>" }
+    static var libraryVersion: String { "10.0.2" }
 
     var hardware: Hardware {
 
