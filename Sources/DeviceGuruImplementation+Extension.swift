31bbe20c2e1220c296ae8ddfdb9363e7591ef762
Inder <352443+InderKumarRathore@users.noreply.github.com>
2023-10-21 14:48:02 +0800

    Updating the version in file

--- Sources/DeviceGuruImplementation+Extension.swift
+++ Sources/DeviceGuruImplementation+Extension.swift
@@ -2,7 +2,7 @@
 public extension DeviceGuruImplementation {
 
     /// This should be same as cocoa pod version
-    static var libraryVersion: String { "10.0.3" }
+    static var libraryVersion: String { "10.0.4" }
 
     var hardware: Hardware {
 
